05ab6c0236ad322a512857823870d8b207f1eadd
Dominick Chavez <dominickcchav@gmail.com>
2021-05-11 19:15:00 -0500

    Function Constructors

---
+++ Development/Class Activities/Week_5/5.2.1-FunctionConstructors.js
@@ -0,0 +1,22 @@
+// create an object literal
+
+let animal = {
+    name: 'Bruce',
+    hobbies: ['Go to park','run']
+};
+
+// include a method that uses "this"
+
+let fun = {
+    chomp: 'stick',
+    action() {
+        return `${animal.name} likes to ${animal.hobbies[0]}and break ${this.chomp}s.`
+    }
+}
+
+// create a function in the global scope that prints "this"
+
+let text = () => {
+    console.log(fun.action())
+};
+text();
